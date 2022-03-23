69a313dade23467636e62b87f512c926425fc073
timegated <beccdaniel@gmail.com>
2022-03-23 10:16:12 -0700

    Cleaned up CRA boilerplate, clean slate

--- pizza-ui/src/App.test.js
+++ pizza-ui/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders lol pizza', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/lol pizza/i);
   expect(linkElement).toBeInTheDocument();
 });
